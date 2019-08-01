ba24b3ea2ef68556576472db12986c8cd00ad6f7
Colleen <colleenhiggins87@gmail.com>
2019-08-01 09:16:06 -0700

    testing and users post endpoint added

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -3,6 +3,7 @@ const AuthService = require('./auth-service')
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
+
 authRouter
 .post('/login', jsonBodyParser, (req,res,next)=>{
   const { user_name, password} = req.body
